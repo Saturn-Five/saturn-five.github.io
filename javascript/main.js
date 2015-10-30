8ba621e2f5720a12671aa0b3ba45f1b1bcbcde8e
Ardalan <ardalan.samimi@gmail.com>
2015-10-30 16:39:00 +0100

    Responsive design + scrolling navigation && small design change

--- javascript/main.js
+++ javascript/main.js
@@ -1,8 +1,26 @@
-$(document).ready(function (){
+$(document).ready(function () {
+    $(".navigation").hide();
+
     $(".scroller").click(function (e) {
-        // Stop the link execution
         e.preventDefault();
         var anchor = $(this).attr("href");
         $('html,body').animate({ scrollTop: $(anchor).offset().top }, 500);
+    });
+
+    $(window).scroll(function(event) {
+        var scrollTop = $(window).scrollTop();
+        if (scrollTop > $(".splash").height()) {
+            if ($(".navigation").is(':hidden')) {
+                $(".navigation").fadeIn();
+            } else {
+                if (scrollTop > $(".contact").offset().top-$(".apps").height()) {
+                    $(".navigation > a").css("color", "#FFFFFF");
+                } else {
+                    $(".navigation > a").css("color", "#FFFF99");
+                }
+            }
+        } else {
+            $(".navigation").fadeOut();
+        }
     });
 });
